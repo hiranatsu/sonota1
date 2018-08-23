4668108e8a8239636ea00e43d964e75691c627cc
hiranatsu <hideoooh@gmail.com>
2018-08-23 17:54:00 +0900

    20180823-3

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/c2/e0841418a6a600181cfd87bcccfb027c
@@ -0,0 +1,12 @@
+drop database if exists testdb3;
+create database testdb3;
+use testdb3;
+
+create table users(
+user_id int primary key auto_increment,
+user_name varchar(255),
+password varchar(255)
+);
+
+insert into users values(1,"miyata","123");
+insert into users values(2,)
--- WebProj3/sql/testdb.sql
+++ WebProj3/sql/testdb.sql
@@ -1,6 +1,6 @@
-drop database if exists testdb3;
-create database testdb3;
-use testdb3;
+drop database if exists projdb3;
+create database projdb3;
+use projdb3;
 
 create table users(
 user_id int primary key auto_increment,
@@ -9,4 +9,11 @@ password varchar(255)
 );
 
 insert into users values(1,"miyata","123");
-insert into users values(2,)
+insert into users values(2,"nakajima","456");
+insert into users values(3,"yoshida","789");
+
+create table inquiry(
+name varchar(255),
+qtype varchar(255),
+body varchar(255)
+);
